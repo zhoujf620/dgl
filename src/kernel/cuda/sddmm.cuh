46801a9e126a49f3122d03ac6340c2d6258381bd
yzh119 <expye@outlook.com>
2020-05-31 03:59:21 +0800

    op

--- src/kernel/cuda/sddmm.cuh
+++ src/kernel/cuda/sddmm.cuh
@@ -5,11 +5,129 @@
 #ifndef DGL_KERNEL_CUDA_SDDMM_CUH_
 #define DGL_KERNEL_CUDA_SDDMM_CUH_
 
-#include <minigun/minigun.h>
+#include "../graph/unit_graph.h"
+#include "../util.h"
 
 namespace dgl {
 namespace kernel {
 namespace cuda {
+
+template <typename Idx, typename DType,
+          typename BinaryOp, typename ReduceOp>
+__global__ void SDDMMCooKernel(
+  DType *ufeat, DType *vfeat, DType *out,
+  Idx *row, Idx *col, Idx* edge_map,
+  int64_t N, int64_t M, int64_t E, int64_t ndim,
+  int64_t *ubcast_off, int64_t *vbcast_off,
+  int64_t *ufeat_shp, int64_t *vfeat_shp, int64_t *out_shp,
+  int64_t ufeat_stride, int64_t vfeat_stride, int64_t out_stride) {
+  // SDDMM with COO.
+  const Idx ty = blockIdx.y * blockDim.y + threadIdx.y;
+  const Idx stride_y = blockDim.y * gridDim.y;
+  while (ty < E) {
+    const Idx src = _ldg(row + ty);
+    const Idx dst = _ldg(col + ty);
+    const Idx eid = _ldg(edge_map + ty);
+    int64_t tx = blockIdx.x * blockDim.x + threadIdx.x;
+    const int64_t stride_x = blockDim.x * gridDim.x;
+    DType* lhsoff = BinaryOp::UseLhs() ? (ufeat + src * ufeat_stride): nullptr;
+    DType* rhsoff = BinaryOp::UseRhs() ? (vfeat + dst * vfeat_stride): nullptr;
+    DType* outoff = out + dst * out_stride;
+    while (tx < out_stride) {
+      DType val = BinaryOp::Call(
+          lhsoff + ubcast_off[tx] * BinaryOp::ReduceSize(ufeat_shp, ndim),
+          rhsoff + vbcast_off[tx] * BinaryOp::ReduceSize(vfeat_shp, ndim));
+      outoff[tx] = val;
+      tx += stride_x;
+    }
+    ty += stride_y;
+  }
+}
+
+// Binary search the row_offsets to find the source node of the edge id.
+template <typename Idx>
+__device__ __forceinline__ Idx BinarySearchSrc(Idx *array, Idx length, Idx eid) {
+  Idx lo = 0, hi = length - 1;
+  while (lo < hi) {
+    Idx mid = (lo + hi) >> 1;
+    if (_ldg(array + mid) <= eid) {
+      lo = mid + 1;
+    } else {
+      hi = mid;
+    }
+  }
+  // INVARIANT: lo == hi
+  if (_ldg(array + hi) == eid) {
+    return hi;
+  } else {
+    return hi - 1;
+  }
+}
+
+template <typename Idx, typename DType,
+          typename BinaryOp, typename ReduceOp>
+__global__ void SDDMMCsrKernel(
+  DType *ufeat, DType *vfeat, DType *out,
+  Idx *indptr, Idx *indices, Idx* edge_map,
+  int64_t N, int64_t M, int64_t E, int64_t ndim,
+  int64_t *ubcast_off, int64_t *vbcast_off,
+  int64_t *ufeat_shp, int64_t *vfeat_shp, int64_t *out_shp,
+  int64_t ufeat_stride, int64_t vfeat_stride, int64_t out_stride) {
+  // SDDMM with Csr.
+  const Idx ty = blockIdx.y * blockDim.y + threadIdx.y;
+  const Idx stride_y = blockDim.y * gridDim.y;
+  while (ty < E) {
+    const Idx src = BinarySearchSrc(indptr, N, ty);
+    const Idx dst = _ldg(indices + ty);
+    const Idx eid = _ldg(edge_map + ty);
+    int64_t tx = blockIdx.x * blockDim.x + threadIdx.x;
+    const int64_t stride_x = blockDim.x * gridDim.x;
+    DType* lhsoff = BinaryOp::UseLhs() ? (ufeat + src * ufeat_stride): nullptr;
+    DType* rhsoff = BinaryOp::UseRhs() ? (vfeat + dst * vfeat_stride): nullptr;
+    DType* outoff = out + dst * out_stride;
+    while (tx < out_stride) {
+      DType val = BinaryOp::Call(
+          lhsoff + ubcast_off[tx] * BinaryOp::ReduceSize(ufeat_shp, ndim),
+          rhsoff + vbcast_off[tx] * BinaryOp::ReduceSize(vfeat_shp, ndim));
+      outoff[tx] = val;
+      tx += stride_x;
+    }
+    ty += stride_y;
+  }
+}
+
+template <typename Idx, typename DType,
+          typename BinaryOp, typename ReduceOp>
+void CudaSDDMMCoo(
+    dgl::aten::COOMatrix coo,
+    NDArray ufeat,
+    NDArray vfeat,
+    NDArray out) {
+  // TODO(zihao)
+}
+
+template <typename Idx, typename DType,
+          typename BinaryOp, typename ReduceOp>
+void CudaSDDMMCsr(
+    dgl::aten::CSRMatrix csr,
+    NDArray ufeat,
+    NDArray vfeat,
+    NDArray out) {
+  // TODO(zihao)
+}
+
+template <typename Idx, typename DType,
+          typename BinaryOp, typename ReduceOp>
+void CudaCallSDDMM(
+  const UnitGraph* graph,
+  NDArray ufeat,
+  NDArray vfeat,
+  NDArray out,
+  SparseFormat preferred_format = SparseFormat::kCoo,
+  ) {
+  // TODO(zihao)
+}
+
 
 }
 }
--- src/kernel/cuda/spmm.cuh
+++ src/kernel/cuda/spmm.cuh
@@ -5,7 +5,6 @@
 #ifndef DGL_KERNEL_CUDA_SPMM_CUH_
 #define DGL_KERNEL_CUDA_SPMM_CUH_
 
-#include <minigun/minigun.h>
 #include "../graph/unit_graph.h"
 #include "../util.h"
 
@@ -64,12 +63,12 @@ __device__ __forceinline__ void UnravelRavel(
 template <typename Idx, typename DType,
           typename BinaryOp, typename ReduceOp>
 __global__ void SpMMCooKernel(
-  DType *nfeat, DType *efeat, DType *out, Idx *arg_out_n, Idx *arg_out_e,
+  DType *ufeat, DType *efeat, DType *out, Idx *arg_u, Idx *arg_e,
   Idx *row, Idx *col, Idx* edge_map,
   int64_t N, int64_t M, int64_t E, int64_t ndim,
-  int64_t *nbcast_off, int64_t *ebcast_off,
-  int64_t *nfeat_shp, int64_t *efeat_shp, int64_t *out_shp,
-  int64_t nfeat_stride, int64_t efeat_stride, int64_t out_stride) {
+  int64_t *ubcast_off, int64_t *ebcast_off,
+  int64_t *ufeat_shp, int64_t *efeat_shp, int64_t *out_shp,
+  int64_t ufeat_stride, int64_t efeat_stride, int64_t out_stride) {
   // SPMM with COO.
   const Idx ty = blockIdx.y * blockDim.y + threadIdx.y;
   const Idx stride_y = blockDim.y * gridDim.y;
@@ -77,31 +76,61 @@ __global__ void SpMMCooKernel(
     const Idx src = _ldg(row + ty);
     const Idx dst = _ldg(col + ty);
     const Idx eid = _ldg(edge_map + ty);
-    {
-      int64_t tx = blockIdx.x * blockDim.x + threadIdx.x;
-      const int64_t stride_x = blockDim.x * gridDim.x;
-      DType* lhsoff = BinaryOp::UseNode() ? (nfeat + src * nfeat_stride): nullptr;
-      DType* rhsoff = BinaryOp::UseEdge() ? (efeat + eid * efeat_stride): nullptr;
-      DType* outoff = out + dst * out_stride;
-      Idx* outargnoff = ReduceOp::RequireArgX() ? (arg_out_n + dst * out_stride): nullptr;
-      Idx* outargeoff = ReduceOp::RequireArgY() ? (arg_out_e + dst * out_stride): nullptr;
-      while (tx < out_stride) {
-        DType out = BinaryOp::Call(lhsoff + nbcast_off[tx], rhsoff + ebcast_off[tx]);
-        ReduceOp::Call(tx, outoff, outargnoff, outargeoff, out, src, eid);
-        tx += stride_x;
-      }
+    int64_t tx = blockIdx.x * blockDim.x + threadIdx.x;
+    const int64_t stride_x = blockDim.x * gridDim.x;
+    DType* uoff = BinaryOp::UseLhs() ? (ufeat + src * ufeat_stride): nullptr;
+    DType* eoff = BinaryOp::UseRhs() ? (efeat + eid * efeat_stride): nullptr;
+    DType* outoff = out + dst * out_stride;
+    Idx* arguoff = (ReduceOp::RequireArg() && BinaryOp::UseLhs()) ? (arg_u + dst * out_stride): nullptr;
+    Idx* argeoff = (ReduceOp::RequireArg() && BinaryOp::UseRhs()) ? (arg_e + dst * out_stride): nullptr;
+    while (tx < out_stride) {
+      DType val = BinaryOp::Call(uoff + ubcast_off[tx], eoff + ebcast_off[tx]);
+      ReduceOp::Call(tx, outoff, arguoff, argeoff, val, src, eid);
+      tx += stride_x;
+    }
+    ty += stride_y;
+  }
+}
+
+template <typename Idx, typename DType,
+          typename BinaryOp, typename ReduceOp>
+__global__ void ArgSpMMCooKernel(
+  DType *ufeat, DType *efeat, DType *out, Idx *arg_u, Idx *arg_e,
+  Idx *row, Idx *col, Idx* edge_map,
+  int64_t N, int64_t M, int64_t E, int64_t ndim,
+  int64_t *ubcast_off, int64_t *ebcast_off,
+  int64_t *ufeat_shp, int64_t *efeat_shp, int64_t *out_shp,
+  int64_t ufeat_stride, int64_t efeat_stride, int64_t out_stride) {
+  // SPMM with COO.
+  const Idx ty = blockIdx.y * blockDim.y + threadIdx.y;
+  const Idx stride_y = blockDim.y * gridDim.y;
+  while (ty < E) {
+    const Idx src = _ldg(row + ty);
+    const Idx dst = _ldg(col + ty);
+    const Idx eid = _ldg(edge_map + ty);
+    int64_t tx = blockIdx.x * blockDim.x + threadIdx.x;
+    const int64_t stride_x = blockDim.x * gridDim.x;
+    DType* uoff = BinaryOp::UseLhs() ? (ufeat + src * ufeat_stride): nullptr;
+    DType* eoff = BinaryOp::UseRhs() ? (efeat + eid * efeat_stride): nullptr;
+    DType* outoff = out + dst * out_stride;
+    Idx* arguoff = BinaryOp::UseLhs() ? (arg_u + dst * out_stride): nullptr;
+    Idx* argeoff = BinaryOp::UseRhs() ? (arg_e + dst * out_stride): nullptr;
+    while (tx < out_stride) {
+      DType val = BinaryOp::Call(uoff + ubcast_off[tx], eoff + ebcast_off[tx]);
+      ReduceOp::CallArg(tx, arguoff, argeoff, val, outoff[tx], src, eid);
+      tx += stride_x;
     }
     ty += stride_y;
   }
 }
 
 __global__ void SpMMCsrKernel(
-  DType *nfeat, DType *efeat, DType *out, Idx *arg_out_n, Idx *arg_out_e,
+  DType *ufeat, DType *efeat, DType *out, Idx *arg_u, Idx *arg_e,
   Idx *indptr, Idx *indices, Idx *edge_map,
   int64_t N, int64_t M, int64_t E, int64_t ndim,
-  int64_t *nbcast_off, int64_t *ebcast_off,
-  int64_t *nfeat_shp, int64_t *efeat_shp, int64_t *out_shp,
-  int64_t nfeat_stride, int64_t efeat_stride, int64_t out_stride) {
+  int64_t *ubcast_off, int64_t *ebcast_off,
+  int64_t *ufeat_shp, int64_t *efeat_shp, int64_t *out_shp,
+  int64_t ufeat_stride, int64_t efeat_stride, int64_t out_stride) {
   // SPMM with COO.
   const Idx ty = blockIdx.y * blockDim.y + threadIdx.y;
   const Idx stride_y = blockDim.y * gridDim.y;
@@ -113,16 +142,14 @@ __global__ void SpMMCsrKernel(
       const Idx src = _ldg(indptr + i);
       int64_t tx = blockIdx.x * blockDim.x + threadIdx.x;
       const int64_t stride_x = blockDim.x * gridDim.x;
-      DType* lhsoff = BinaryOp::UseNode() ? (nfeat + src * nfeat_stride): nullptr;
-      DType* rhsoff = BinaryOp::UseEdge() ? (efeat + eid * efeat_stride): nullptr;
+      DType* uoff = BinaryOp::UseLhs() ? (ufeat + src * ufeat_stride): nullptr;
+      DType* eoff = BinaryOp::UseRhs() ? (efeat + eid * efeat_stride): nullptr;
       DType* outoff = out + dst * out_stride;
-      Idx* outargnoff = ReduceOp::RequireArgX() ? (arg_out_n + dst * out_stride): nullptr;
-      Idx* outargeoff = ReduceOp::RequireArgY() ? (arg_out_e + dst * out_stride): nullptr;
+      Idx* arguoff = (ReduceOp::RequireArg() && BinaryOp::UseLhs()) ? (arg_u + dst * out_stride): nullptr;
+      Idx* argeoff = (ReduceOp::RequireArg() && BinaryOp::UseRhs()) ? (arg_e + dst * out_stride): nullptr;
       while (tx < out_stride) {
-        int64_t lhs_add = nbcast_off[tx];
-        int64_t rhs_add = ebcast_off[tx];
-        DType out = BinaryOp::Call(lhsoff + lhs_add, rhsoff + rhs_ad);
-        ReduceOp::Call(tx, outoff, outargnoff, outargeoff, out, src, eid);
+        DType out = BinaryOp::Call(uoff + ubcast_off[tx], eoff + ebcast_off[tx]);
+        ReduceOp::Call(tx, outoff, arguoff, argeoff, out, src, eid);
         tx += stride_x;
       }
     }
@@ -132,9 +159,9 @@ __global__ void SpMMCsrKernel(
 
 template <typename Idx, typename DType,
           typename BinaryOp, typename ReduceOp>
-void SpMMCoo(
+void CudaSpMMCoo(
     dgl::aten::COOMatrix coo,
-    NDArray nfeat,
+    NDArray ufeat,
     NDArray efeat,
     NDArray out) {
   // TODO(zihao)
@@ -142,25 +169,229 @@ void SpMMCoo(
 
 template <typename Idx, typename DType,
           typename BinaryOp, typename ReduceOp>
-void SpMMCsr(
+void CudaSpMMCsr(
     dgl::aten::CSRMatrix csr,
-    NDArray nfeat,
+    NDArray ufeat,
     NDArray efeat,
     NDArray out) {
   // TODO(zihao)
 }
 
 template <typename Idx, typename DType,
-          typename ReduceOp>
-void CallSPMM(
+          typename BinaryOp, typename ReduceOp>
+void CudaCallSpMM(
   const UnitGraph* graph,
-  NDArray nfeat,
+  NDArray ufeat,
   NDArray efeat,
   NDArray out,
-  const SparseFormat preferred_format = SparseFormat::kCsc,
+  SparseFormat preferred_format = SparseFormat::kCsc,
   ) {
   // TODO(zihao)
-)
+}
+
+namespace binary {
+template <typename DType>
+struct Add {
+  static __device__ __forceinline__ void Call(
+      DType *lhs, DType *rhs, int64_t len_lhs = 1, int64_t len_rhs = 1) {
+    return lhs[0] + rhs[0];
+  }
+  static __device__ __forceinline__ bool UseLhs() {
+    return true;
+  }
+  static __device__ __forceinline__ bool UseRhs() {
+    return true;
+  }
+  static __device__ __forceinline__ bool ReduceDim() {
+    return false;
+  }
+  static __device__ __forceinline__ int64_t ReduceSize(int64_t *feat_shp, int64_t ndim) {
+    return 1;
+  }
+};
+
+template <typename DType>
+struct Mul {
+  static __device__ __forceinline__ void Call(
+      DType *lhs, DType *rhs, int64_t len_lhs = 1, int64_t len_rhs = 1) {
+    return lhs[0] * rhs[0];
+  }
+  static __device__ __forceinline__ bool UseLhs() {
+    return true;
+  }
+  static __device__ __forceinline__ bool UseRhs() {
+    return true;
+  }
+  static __device__ __forceinline__ bool ReduceDim() {
+    return false;
+  }
+  static __device__ __forceinline__ int64_t ReduceSize(int64_t *feat_shp, int64_t ndim) {
+    return 1;
+  }
+};
+
+template <typename DType>
+struct CopyU {
+  static __device__ __forceinline__ void Call(
+      DType *lhs, DType *rhs, int64_t len_lhs = 1, int64_t len_rhs = 1) {
+    return lhs[0];
+  }
+  static __device__ __forceinline__ bool UseLhs() {
+    return true;
+  }
+  static __device__ __forceinline__ bool UseRhs() {
+    return false;
+  }
+  static __device__ __forceinline__ bool ReduceDim() {
+    return false;
+  }
+  static __device__ __forceinline__ int64_t ReduceSize(int64_t *feat_shp, int64_t ndim) {
+    return 1;
+  }
+};
+
+template <typename DType>
+struct CopyE {
+  static __device__ __forceinline__ void Call(
+      DType *lhs, DType *rhs, int64_t len_lhs = 1, int64_t len_rhs = 1) {
+    return rhs[0];
+  }
+  static __device__ __forceinline__ bool UseLhs() {
+    return false;
+  }
+  static __device__ __forceinline__ bool UseRhs() {
+    return true;
+  }
+  static __device__ __forceinline__ bool ReduceDim() {
+    return false;
+  }
+  static __device__ __forceinline__ int64_t ReduceSize(int64_t *feat_shp, int64_t ndim) {
+    return 1;
+  }
+};
+
+template <typename DType>
+struct Dot {
+  static __device__ __forceinline__ void Call(
+      DType *lhs, DType *rhs, int64_t len_lhs = 1, int64_t len_rhs = 1) {
+    DType rst = static_cast<DType>(0);
+    for (int64_t i = 0; i < max(len_lhs, len_rhs); ++i) {
+      rst += lhs[min(i, len_lhs - 1)] * rhs[min(i, len_rhs - 1)];
+    }
+    return rst;
+  }
+  static __device__ __forceinline__ bool UseLhs() {
+    return true;
+  }
+  static __device__ __forceinline__ bool UseRhs() {
+    return true;
+  }
+  static __device__ __forceinline__ bool ReduceDim() {
+    return true;
+  }
+  static __device__ __forceinline__ int64_t ReduceSize(int64_t *feat_shp, int64_t ndim) {
+    return feat_shp[ndim - 1];
+  }
+};
+
+
+}   // end of namespace binary
+
+namespace reduce {
+template <typename Idx,
+          typename DType,
+          bool atomic=false>
+struct Sum {
+  static __device__ __forceinline__ void Call(Idx fid,
+    DType *out_buf, Idx *arg_u_buf, Idx *arg_e_buf,
+    DType val, Idx uid, Idx eid) {
+    if (!atomic) {
+      out_buf[fid] += val;
+    } else {
+      cuda::AtomicAdd(out_buf + fid, val);
+    }
+  }
+  static __device__ __forceinline__ bool RequireArg() {
+    return false;
+  }
+  static __device__ __forceinline__ void CallArg(Idx fid,
+    DType *arg_u_buf, DType *arg_e_buf,
+    DType val, DType val_ref, Idx uid, Idx eid) {
+      // placeholder
+    }
+};
+
+template <typename Idx,
+          typename DType,
+          bool atomic=false>
+struct Max {
+  static __device__ __forceinline__ void Call(Idx fid,
+    DType *out_buf, Idx *arg_u_buf, Idx *arg_e_buf,
+    DType val, Idx uid, Idx eid) {
+    if (!atomic) {
+      Idx max_val = max(out_buf[fid], val);
+      if (max_val == val) {
+        out_buf[fid] = max_val;
+        arg_u_buf[fid] = uid;
+        arg_e_buf[fid] = eid;
+      }
+    } else {
+      cuda::AtomicMax(out_buf + fid, val);
+    }
+  }
+  static __device__ __forceinline__ bool RequireArg() {
+    return true;
+  }
+  static __device__ __forceinline__ void CallArg(Idx fid,
+    DType *arg_u_buf, DType *arg_e_buf,
+    DType val, DType val_ref, Idx uid, Idx eid) {
+    if (atomic) {
+      if (val == val_ref) {
+        if (arg_u_buf)
+          arg_u_buf[fid] = uid; // TODO(zihao): select min?
+        if (arg_e_buf)
+          arg_e_buf[fid] = eid;
+      }
+    }
+  }
+};
+
+template <typename Idx,
+          typename DType,
+          bool atomic=false>
+struct Min {
+  static __device__ __forceinline__ void Call(Idx fid,
+    DType *out_buf, Idx *arg_u_buf, Idx *arg_e_buf,
+    DType val, Idx uid, Idx eid) {
+    if (!atomic) {
+      Idx min_val = min(out_buf[fid], val);
+      if (min_val == val) {
+        out_buf[fid] = min_val;
+        arg_u_buf[fid] = uid;
+        arg_e_buf[fid] = eid;
+      }
+    } else {
+      cuda::AtomicMin(out_buf + fid, val);
+    }
+  }
+  static __device__ __forceinline__ bool RequireArg() {
+    return true;
+  }
+  static __device__ __forceinline__ void CallArg(Idx fid,
+    DType *arg_u_buf, DType *arg_e_buf,
+    DType val, DType val_ref, Idx uid, Idx eid) {
+    if (atomic) {
+      if (val == val_ref) {
+        if (arg_u_buf)
+          arg_u_buf[fid] = uid; // TODO(zihao): select min?
+        if (arg_e_buf)
+          arg_e_buf[fid] = eid;
+      }
+    }
+  }
+};
+
+}   // end of namespace reduce
 
 
 }
