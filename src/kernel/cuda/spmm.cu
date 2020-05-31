c7b74d221c69d7430f3b262675c0f4b10c0095cb
yzh119 <expye@outlook.com>
2020-05-31 21:12:23 +0800

    upd

--- src/kernel/cuda/spmm.cu
+++ src/kernel/cuda/spmm.cu
@@ -1,5 +1,6 @@
 //#include "./spmm.cuh"
 #include <dgl/array.h>
+#include "../../runtime/cuda/cuda_common.h"
 
 namespace dgl {
 namespace kernel {
@@ -51,6 +52,164 @@ template void SpMMCoo<kDLGPU, int32_t, double>(
 template void SpMMCoo<kDLGPU, int64_t, double>(
     const std::string& op, const std::string& reduce, const aten::COOMatrix& coo,
     NDArray ufeat, NDArray efeat, NDArray out, std::vector<NDArray> out_aux);
+
+namespace cusparse {
+
+template <typename DType>
+cusparseStatus_t Xcsrmm2(cusparseHandle_t handle, cusparseOperation_t transA,
+    cusparseOperation_t transB, int m, int n, int k, int nnz,
+    const DType* alpha, const cusparseMatDescr_t descrA,
+    const DType* csrValA, const int* csrRowPtrA, const int* csrColIndA,
+    const DType* B, int ldb, const DType* beta, DType* C, int ldc) {
+  LOG(INFO) << "Not supported dtype";
+  return CUSPARSE_STATUS_EXECUTION_FAILED;
+}
+
+template <>
+cusparseStatus_t Xcsrmm2<float>(cusparseHandle_t handle, cusparseOperation_t transA,
+    cusparseOperation_t transB, int m, int n, int k, int nnz,
+    const float* alpha, const cusparseMatDescr_t descrA,
+    const float* csrValA, const int* csrRowPtrA, const int* csrColIndA,
+    const float* B, int ldb, const float* beta, float* C, int ldc) {
+  return cusparseScsrmm2(handle, transA, transB, m, n, k, nnz,
+      alpha, descrA, csrValA, csrRowPtrA, csrColIndA,
+      B, ldb, beta, C, ldc);
+}
+
+template <>
+cusparseStatus_t Xcsrmm2<double>(cusparseHandle_t handle, cusparseOperation_t transA,
+    cusparseOperation_t transB, int m, int n, int k, int nnz,
+    const double* alpha, const cusparseMatDescr_t descrA,
+    const double* csrValA, const int* csrRowPtrA, const int* csrColIndA,
+    const double* B, int ldb, const double* beta, double* C, int ldc) {
+  return cusparseDcsrmm2(handle, transA, transB, m, n, k, nnz,
+      alpha, descrA, csrValA, csrRowPtrA, csrColIndA,
+      B, ldb, beta, C, ldc);
+}
+
+template <typename DType>
+cublasStatus_t Xgeam(cublasHandle_t handle, cublasOperation_t transa,
+    cublasOperation_t transb, int m, int n,
+    const DType* alpha, const DType* A, int lda,
+    const DType* beta, const DType* B, int ldb,
+    DType* C, int ldc) {
+  LOG(INFO) << "Not supported dtype";
+  return CUBLAS_STATUS_EXECUTION_FAILED;
+}
+
+template <>
+cublasStatus_t Xgeam<float>(cublasHandle_t handle, cublasOperation_t transa,
+    cublasOperation_t transb, int m, int n,
+    const float* alpha, const float* A, int lda,
+    const float* beta, const float* B, int ldb,
+    float* C, int ldc) {
+  return cublasSgeam(handle, transa, transb, m, n, alpha, A, lda,
+      beta, B, ldb, C, ldc);
+}
+
+template <>
+cublasStatus_t Xgeam<double>(cublasHandle_t handle, cublasOperation_t transa,
+    cublasOperation_t transb, int m, int n,
+    const double* alpha, const double* A, int lda,
+    const double* beta, const double* B, int ldb,
+    double* C, int ldc) {
+  return cublasDgeam(handle, transa, transb, m, n, alpha, A, lda,
+      beta, B, ldb, C, ldc);
+}
+
+template <typename DType>
+void CusparseCsrmm2(
+    const RuntimeConfig& rtcfg,
+    const aten::CSRMatrix& csr,
+    const DType* B_data, DType* C_data,
+    int x_length) {
+  // We use csrmm2 to perform following operation:
+  // C = A x B, where A is a sparse matrix in csr format, B is the dense matrix for node
+  // feature tensor. However, since cusparse only supports column-major, while our tensor
+  // is stored in row-major, the actual computation is:
+  // C = trans(A x trans(B)).
+  // Currently, we use cublasXgeam to implement transposition and allocate intermediate
+  // workspace memory for this.
+  const int m = csr.num_rows;
+  const int n = x_length;
+  const int k = csr.num_cols;
+  const int nnz = csr.indices->shape[0];
+  const DType alpha = 1.0;
+  const DType beta = 0.0;
+  // device
+  auto device = runtime::DeviceAPI::Get(rtcfg.ctx);
+  auto* thr_entry = runtime::CUDAThreadEntry::ThreadLocal();
+  // allocate cusparse handle if needed
+  if (!thr_entry->cusparse_handle) {
+    CUSPARSE_CALL(cusparseCreate(&(thr_entry->cusparse_handle)));
+  }
+  CUSPARSE_CALL(cusparseSetStream(thr_entry->cusparse_handle, rtcfg.stream));
+  // allocate matrix for temporary transposed output
+  DType* trans_out = static_cast<DType*>(device->AllocWorkspace(rtcfg.ctx, m * n * sizeof(DType)));
+  // all one data array
+  DType* valptr = static_cast<DType*>(device->AllocWorkspace(rtcfg.ctx, nnz * sizeof(DType)));
+  utils::Fill<kDLGPU>(rtcfg.ctx, valptr, nnz, static_cast<DType>(1.));
+  cusparseMatDescr_t descr;
+  CUSPARSE_CALL(cusparseCreateMatDescr(&descr));
+  CUSPARSE_CALL(cusparseSetMatType(descr, CUSPARSE_MATRIX_TYPE_GENERAL));
+  CUSPARSE_CALL(cusparseSetMatIndexBase(descr, CUSPARSE_INDEX_BASE_ZERO));
+  CUSPARSE_CALL(Xcsrmm2<DType>(
+      thr_entry->cusparse_handle,
+      CUSPARSE_OPERATION_NON_TRANSPOSE,
+      CUSPARSE_OPERATION_TRANSPOSE,
+      m, n, k, nnz, &alpha,
+      descr, valptr,
+      static_cast<int32_t*>(csr.indptr->data),
+      static_cast<int32_t*>(csr.indices->data),
+      B_data, n, &beta, trans_out, m));
+  device->FreeWorkspace(rtcfg.ctx, valptr);
+  // transpose the output matrix
+  if (!thr_entry->cublas_handle) {
+    CUBLAS_CALL(cublasCreate(&(thr_entry->cublas_handle)));
+  }
+  CUBLAS_CALL(cublasSetStream(thr_entry->cublas_handle, rtcfg.stream));
+  CUBLAS_CALL(Xgeam<DType>(
+      thr_entry->cublas_handle,
+      CUBLAS_OP_T,
+      CUBLAS_OP_N,
+      n, m,
+      &alpha, trans_out, m,
+      &beta, nullptr, n,
+      C_data, n));
+  device->FreeWorkspace(rtcfg.ctx, trans_out);
+}
+
+}  // namespace cusparse
+
+template <int32_t, float>
+void SpMMSumCsr(const aten::CSRMatrix& csr,
+                NDArray ufeat, NDArray efeat,
+                NDArray out) {
+  minigun::advance::RuntimeConfig rtcfg;
+  rtcfg.ctx = out->ctx;
+  DType *udata = static_cast<float*>(ufeat->data);
+  DType *edata = static_cast<float*>(efeat->data);
+  const int64_t len = out->shape[1];
+  cusparse::CusparseCsrmm2(rtcfg, csr, edata, udata, len);
+}
+
+template <int32_t, double>
+void SpMMSumCsr(const aten::CSRMatrix& csr,
+                NDArray ufeat, NDArray efeat,
+                NDArray out) {
+  minigun::advance::RuntimeConfig rtcfg;
+  rtcfg.ctx = out->ctx;
+  DType *udata = static_cast<double*>(ufeat->data);
+  DType *edata = static_cast<double*>(efeat->data);
+  const int64_t len = out->shape[1];
+  cusparse::CusparseCsrmm2(rtcfg, csr, edata, udata, len);
+}
+
+template <typename IdType, typename DType, typename Op>
+void SpMMSumCsr(const aten::CSRMatrix& csr,
+                NDArray ufeat, NDArray efeat,
+                NDArray out) {}
+
 
 }  // namespace kernel
 }  // namespace dgl
--- src/kernel/cuda/spmm.cuh
+++ src/kernel/cuda/spmm.cuh
@@ -20,14 +20,14 @@ namespace cuda {
  *   3. Convert multi-dimension index to flattened index for rhs.
  */
 __device__ __forceinline__ void UnravelRavel(
-    const int64_t idx, const int ndim, const int64_t* out_shape, const int64_t* out_stride,
+    const int64_t idx, const int ndim, const int64_t* out_shape, const int64_t* out_len,
     const int64_t* lhs_shape, const int64_t* lhs_stride,
     const int64_t* rhs_shape, const int64_t* rhs_stride,
     int64_t *lhs_out, int64_t *rhs_out) {
-  if (out_stride[0] == lhs_stride[0]) {
+  if (out_len[0] == lhs_stride[0]) {
     for (int d = 0; d < ndim; ++d) {
       int64_t o_sh = out_shape[d];
-      int64_t o_st = out_stride[d];
+      int64_t o_st = out_len[d];
       int64_t rhs_sh = rhs_shape[d];
       int64_t rhs_st = rhs_stride[d];
       int64_t i = (idx / o_st) % o_sh;
@@ -43,7 +43,7 @@ __device__ __forceinline__ void UnravelRavel(
   } else {
     for (int d = 0; d < ndim; ++d) {
       int64_t o_sh = out_shape[d];
-      int64_t o_st = out_stride[d];
+      int64_t o_st = out_len[d];
       int64_t lhs_sh = lhs_shape[d];
       int64_t lhs_st = lhs_stride[d];
 
@@ -68,23 +68,26 @@ __global__ void SpMMCooKernel(
   int64_t N, int64_t M, int64_t E, int64_t ndim,
   int64_t *ubcast_off, int64_t *ebcast_off,
   int64_t *ufeat_shp, int64_t *efeat_shp, int64_t *out_shp,
-  int64_t ufeat_stride, int64_t efeat_stride, int64_t out_stride) {
+  int64_t ufeat_len, int64_t efeat_len, int64_t out_len) {
   // SPMM with COO.
+  const bool has_idx = !aten::IsNullArray(csr.data);
   const Idx ty = blockIdx.y * blockDim.y + threadIdx.y;
   const Idx stride_y = blockDim.y * gridDim.y;
   while (ty < E) {
     const Idx src = _ldg(row + ty);
     const Idx dst = _ldg(col + ty);
-    const Idx eid = _ldg(edge_map + ty);
+    const Idx eid = has_idx ? _ldg(edge_map + ty) : ty;
     int64_t tx = blockIdx.x * blockDim.x + threadIdx.x;
     const int64_t stride_x = blockDim.x * gridDim.x;
-    DType* uoff = BinaryOp::UseLhs() ? (ufeat + src * ufeat_stride): nullptr;
-    DType* eoff = BinaryOp::UseRhs() ? (efeat + eid * efeat_stride): nullptr;
-    DType* outoff = out + dst * out_stride;
-    Idx* arguoff = (ReduceOp::RequireArg() && BinaryOp::UseLhs()) ? (arg_u + dst * out_stride): nullptr;
-    Idx* argeoff = (ReduceOp::RequireArg() && BinaryOp::UseRhs()) ? (arg_e + dst * out_stride): nullptr;
-    while (tx < out_stride) {
-      DType val = BinaryOp::Call(uoff + ubcast_off[tx], eoff + ebcast_off[tx]);
+    DType* uoff = BinaryOp::UseLhs() ? (ufeat + src * ufeat_len): nullptr;
+    DType* eoff = BinaryOp::UseRhs() ? (efeat + eid * efeat_len): nullptr;
+    DType* outoff = out + dst * out_len;
+    Idx* arguoff = (ReduceOp::RequireArg() && BinaryOp::UseLhs()) ? (arg_u + dst * out_len): nullptr;
+    Idx* argeoff = (ReduceOp::RequireArg() && BinaryOp::UseRhs()) ? (arg_e + dst * out_len): nullptr;
+    while (tx < out_len) {
+      int64_t lhs_add = ubcast_off ? ubcast_off[tx] : tx;
+      int64_t rhs_add = ebcast_off ? ebcast_off[tx] : tx;
+      DType val = BinaryOp::Call(uoff + lhs_add, eoff + rhs_add);
       ReduceOp::Call(tx, outoff, arguoff, argeoff, val, src, eid);
       tx += stride_x;
     }
@@ -100,23 +103,26 @@ __global__ void ArgSpMMCooKernel(
   int64_t N, int64_t M, int64_t E, int64_t ndim,
   int64_t *ubcast_off, int64_t *ebcast_off,
   int64_t *ufeat_shp, int64_t *efeat_shp, int64_t *out_shp,
-  int64_t ufeat_stride, int64_t efeat_stride, int64_t out_stride) {
-  // SPMM with COO.
+  int64_t ufeat_len, int64_t efeat_len, int64_t out_len) {
+  // SPMM with COO arg max/min.
+  const bool has_idx = !aten::IsNullArray(csr.data);
   const Idx ty = blockIdx.y * blockDim.y + threadIdx.y;
   const Idx stride_y = blockDim.y * gridDim.y;
   while (ty < E) {
     const Idx src = _ldg(row + ty);
     const Idx dst = _ldg(col + ty);
-    const Idx eid = _ldg(edge_map + ty);
+    const Idx eid = has_idx ? _ldg(edge_map + ty) : ty;
     int64_t tx = blockIdx.x * blockDim.x + threadIdx.x;
     const int64_t stride_x = blockDim.x * gridDim.x;
-    DType* uoff = BinaryOp::UseLhs() ? (ufeat + src * ufeat_stride): nullptr;
-    DType* eoff = BinaryOp::UseRhs() ? (efeat + eid * efeat_stride): nullptr;
-    DType* outoff = out + dst * out_stride;
-    Idx* arguoff = BinaryOp::UseLhs() ? (arg_u + dst * out_stride): nullptr;
-    Idx* argeoff = BinaryOp::UseRhs() ? (arg_e + dst * out_stride): nullptr;
-    while (tx < out_stride) {
-      DType val = BinaryOp::Call(uoff + ubcast_off[tx], eoff + ebcast_off[tx]);
+    DType* uoff = BinaryOp::UseLhs() ? (ufeat + src * ufeat_len): nullptr;
+    DType* eoff = BinaryOp::UseRhs() ? (efeat + eid * efeat_len): nullptr;
+    DType* outoff = out + dst * out_len;
+    Idx* arguoff = BinaryOp::UseLhs() ? (arg_u + dst * out_len): nullptr;
+    Idx* argeoff = BinaryOp::UseRhs() ? (arg_e + dst * out_len): nullptr;
+    while (tx < out_len) {
+      int64_t lhs_add = ubcast_off ? ubcast_off[tx] : tx;
+      int64_t rhs_add = ebcast_off ? ebcast_off[tx] : tx;
+      DType val = BinaryOp::Call(uoff + lhs_add, eoff + rhs_add);
       ReduceOp::CallArg(tx, arguoff, argeoff, val, outoff[tx], src, eid);
       tx += stride_x;
     }
@@ -130,25 +136,28 @@ __global__ void SpMMCsrKernel(
   int64_t N, int64_t M, int64_t E, int64_t ndim,
   int64_t *ubcast_off, int64_t *ebcast_off,
   int64_t *ufeat_shp, int64_t *efeat_shp, int64_t *out_shp,
-  int64_t ufeat_stride, int64_t efeat_stride, int64_t out_stride) {
+  int64_t *ufeat_len, int64_t *efeat_len, int64_t *out_len) {
   // SPMM with COO.
+  const bool has_idx = !aten::IsNullArray(csr.data);
   const Idx ty = blockIdx.y * blockDim.y + threadIdx.y;
   const Idx stride_y = blockDim.y * gridDim.y;
   Idx start = 0, end = indptr[dst];
   while (ty < M) {
     const Idx dst = ty;
     for (Idx i = indptr[dst]; i < indptr[dst + 1]; ++i) {
-      const Idx eid = _ldg(edge_map + i);
-      const Idx src = _ldg(indptr + i);
+      const Idx eid = has_idx ? _ldg(edge_map + i) : i;
+      const Idx src = i;
       int64_t tx = blockIdx.x * blockDim.x + threadIdx.x;
       const int64_t stride_x = blockDim.x * gridDim.x;
-      DType* uoff = BinaryOp::UseLhs() ? (ufeat + src * ufeat_stride): nullptr;
-      DType* eoff = BinaryOp::UseRhs() ? (efeat + eid * efeat_stride): nullptr;
-      DType* outoff = out + dst * out_stride;
-      Idx* arguoff = (ReduceOp::RequireArg() && BinaryOp::UseLhs()) ? (arg_u + dst * out_stride): nullptr;
-      Idx* argeoff = (ReduceOp::RequireArg() && BinaryOp::UseRhs()) ? (arg_e + dst * out_stride): nullptr;
-      while (tx < out_stride) {
-        DType out = BinaryOp::Call(uoff + ubcast_off[tx], eoff + ebcast_off[tx]);
+      DType* uoff = BinaryOp::UseLhs() ? (ufeat + src * ufeat_len): nullptr;
+      DType* eoff = BinaryOp::UseRhs() ? (efeat + eid * efeat_len): nullptr;
+      DType* outoff = out + dst * out_len;
+      Idx* arguoff = (ReduceOp::RequireArg() && BinaryOp::UseLhs()) ? (arg_u + dst * out_len): nullptr;
+      Idx* argeoff = (ReduceOp::RequireArg() && BinaryOp::UseRhs()) ? (arg_e + dst * out_len): nullptr;
+      while (tx < out_len) {
+        int64_t lhs_add = ubcast_off ? ubcast_off[tx] : tx;
+        int64_t rhs_add = ebcast_off ? ebcast_off[tx] : tx;
+        DType out = BinaryOp::Call(uoff + lhs_add, eoff + rhs_add);
         ReduceOp::Call(tx, outoff, arguoff, argeoff, out, src, eid);
         tx += stride_x;
       }
@@ -157,14 +166,67 @@ __global__ void SpMMCsrKernel(
   }
 }
 
-template <typename Idx, typename DType,
+template <typename NDim, typename Idx, typename DType,
           typename BinaryOp, typename ReduceOp>
 void CudaSpMMCoo(
     dgl::aten::COOMatrix coo,
     NDArray ufeat,
     NDArray efeat,
-    NDArray out) {
-  // TODO(zihao)
+    NDArray out,
+    BCastInfo info) {
+  Idx *row = coo.row->data, *col = coo.col->data, *edge_map = coo.data->data;
+  DType *ufeat_data = static_cast<DType*>(ufeat->data);
+  DType *efeat_data = static_cast<DType*>(efeat->data);
+  DType *out_data = static_cast<DType*>(out->data);
+  cudaStream_t stream{nullptr};
+  int64_t N = coo.num_rows, M = coo.num_cols, E = coo.row.length;
+  int64_t ndim = info.lhs_shape.size();
+
+  NDArray ufeat_shp, efeat_shp, out_shp;
+  ufeat_shp = NDArray::Empty({ndim}, DLDataType{kDLInt, 64, 1}, DLContext{kDLCPU, 0});
+  efeat_shp = NDArray::Empty({ndim}, DLDataType{kDLInt, 64, 1}, DLContext{kDLCPU, 0});
+  out_shp = NDArray::Empty({ndim}, DLDataType{kDLInt, 64, 1}, DLContext{kDLCPU, 0});
+  int64_t *ufeat_shp_data = static_cast<int64_t*>(ufeat_shp),
+          *efeat_shp_data = static_cast<int64_t*>(efeat_shp),
+          *out_shp_data = static_cast<int64_t*>(out_shp);
+  std::copy(info.lhs_shape.begin(), info.rhs_shape.end(), ufeat_shp_data);
+  std::copy(info.rhs_shape.begin(), info.rhs_shape.end(), efeat_shp_data);
+  std::copy(info.out_shape.begin(), info.rhs_shape.end(), out_shp_data);
+  ufeat_shp = ufeat_shp.CopyTo(ufeat->ctx);
+  efeat_shp = efeat_shp.CopyTo(efeat->ctx);
+  out_shp = out_shp.CopyTo(outfeat->ctx);
+  ufeat_shp_data = static_cast<int64_t*>(ufeat_shp);
+  efeat_shp_data = static_cast<int64_t*>(efeat_shp);
+  out_shp_data = static_cast<int64_t*>(out_shp);
+
+  int64_t *ubcast_off = nullptr, *ebcast_off = nullptr;
+  // ComputeBcastOff(ubcast_off, ebast_off, info);
+  int64_t ufeat_len = utils::Prod(info.lhs_shape);
+  int64_t efeat_len = utils::Prod(info.rhs_shape);
+  int64_t out_len = utils::Prod(info.out_shape);
+  const dim3 nblks(N, 1);
+  const dim3 nthrs(1, 32);
+
+  SpMMCooKernel<Idx, DType, BinaryOp, ReduceOp>
+    <<<nblks, nthrs, 0, stream>>>(
+      ufeat_data, efeat_data, out_data,
+      row, col, edge_map,
+      N, M, E, ndim,
+      ubcast_off, ebcast_off,
+      ufeat_shp_data, efeat_shp_data, out_shp_data,
+      ufeat_len, efeat_len, out_len
+    );
+  if (ReduceOp::RequireArg()) {
+    ArgSpMMCooKernel<Idx, DType, BinaryOp, ReduceOp>
+      <<<nblks, nthrs, 0, stream>>>(
+        ufeat_data, efeat_data, out_data,
+        row, col, edge_map,
+        N, M, E, ndim,
+        ubcast_off, ebcast_off,
+        ufeat_shp_data, efeat_shp_data, out_shp_data,
+        ufeat_len, efeat_len, out_len
+      );
+  }
 }
 
 template <typename Idx, typename DType,
@@ -173,8 +235,52 @@ void CudaSpMMCsr(
     dgl::aten::CSRMatrix csr,
     NDArray ufeat,
     NDArray efeat,
-    NDArray out) {
-  // TODO(zihao)
+    NDArray out,
+    BCastInfo info) {
+  Idx *indptr = csr.indptr->data,
+      *indices = csr.indices->data, 
+      *edge_map = csr.data->data;
+  DType *ufeat_data = static_cast<DType*>(ufeat->data);
+  DType *efeat_data = static_cast<DType*>(efeat->data);
+  DType *out_data = static_cast<DType*>(out->data);
+  cudaStream_t stream{nullptr};
+  int64_t N = coo.num_rows, M = coo.num_cols, E = csr.indices.length;
+  int64_t ndim = info.lhs_shape.size();
+
+  NDArray ufeat_shp, efeat_shp, out_shp;
+  ufeat_shp = NDArray::Empty({ndim}, DLDataType{kDLInt, 64, 1}, DLContext{kDLCPU, 0});
+  efeat_shp = NDArray::Empty({ndim}, DLDataType{kDLInt, 64, 1}, DLContext{kDLCPU, 0});
+  out_shp = NDArray::Empty({ndim}, DLDataType{kDLInt, 64, 1}, DLContext{kDLCPU, 0});
+  int64_t *ufeat_shp_data = static_cast<int64_t*>(ufeat_shp),
+          *efeat_shp_data = static_cast<int64_t*>(efeat_shp),
+          *out_shp_data = static_cast<int64_t*>(out_shp);
+  std::copy(info.lhs_shape.begin(), info.rhs_shape.end(), ufeat_shp_data);
+  std::copy(info.rhs_shape.begin(), info.rhs_shape.end(), efeat_shp_data);
+  std::copy(info.out_shape.begin(), info.rhs_shape.end(), out_shp_data);
+  ufeat_shp = ufeat_shp.CopyTo(ufeat->ctx);
+  efeat_shp = efeat_shp.CopyTo(efeat->ctx);
+  out_shp = out_shp.CopyTo(outfeat->ctx);
+  ufeat_shp_data = static_cast<int64_t*>(ufeat_shp);
+  efeat_shp_data = static_cast<int64_t*>(efeat_shp);
+  out_shp_data = static_cast<int64_t*>(out_shp);
+
+  int64_t *ubcast_off = nullptr, *ebcast_off = nullptr;
+  // ComputeBcastOff(ubcast_off, ebast_off, info);
+  int64_t ufeat_len = utils::Prod(info.lhs_shape);
+  int64_t efeat_len = utils::Prod(info.rhs_shape);
+  int64_t out_len = utils::Prod(info.out_shape);
+  const dim3 nblks(N, 1);
+  const dim3 nthrs(1, 32);
+
+  SpMMCsrKernel<Idx, DType, BinaryOp, ReduceOp>
+    <<<nblks, nthrs, 0, stream>>>(
+      ufeat_data, efeat_data, out_data,
+      indptr, indices, edge_map,
+      N, M, E, ndim,
+      ubcast_off, ebcast_off,
+      ufeat_shp_data, efeat_shp_data, out_shp_data,
+      ufeat_len, efeat_len, out_len
+    );
 }
 
 template <typename Idx, typename DType,
