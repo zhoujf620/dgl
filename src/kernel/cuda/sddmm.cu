3c0555f61c795a386f22f2978d1f72b3b7a0e294
Ubuntu <ubuntu@ip-172-31-26-2.us-east-2.compute.internal>
2020-06-05 11:19:54 +0000

    upd

--- src/kernel/cuda/sddmm.cu
+++ src/kernel/cuda/sddmm.cu
@@ -21,9 +21,6 @@ namespace kernel {
     } else if ((op) == "copy_e") {                                  \
       typedef dgl::kernel::cuda::binary::CopyE<DType> Op;           \
       { __VA_ARGS__ }                                               \
-    } else if ((op) == "dot") {                                  \
-      typedef dgl::kernel::cuda::binary::Dot<DType> Op;           \
-      { __VA_ARGS__ }                                               \
     } else {                                                        \
       LOG(FATAL) << "Unsupported SpMM/SDDMM binary operator: " << op;     \
     }                                                               \
@@ -37,9 +34,33 @@ void SDDMMCsr(const std::string& op,
               NDArray vfeat,
               NDArray out,
               std::vector<NDArray> out_aux) {
-  SWITCH_OP(op, Op, {
-    cuda::SDDMMCsr<IdType, DType, Op>(csr, ufeat, vfeat, out);
-  });
+	if (op == "dot") {
+		int64_t reduce_dim = ufeat->shape[ufeat->ndim - 1];
+		if (reduce_dim <= 2)
+			cuda::SDDMMDotCsr<IdType, DType, 1>(csr, ufeat, vfeat, out);
+		else if (reduce_dim <= 4)
+			cuda::SDDMMDotCsr<IdType, DType, 2>(csr, ufeat, vfeat, out);
+		else if (reduce_dim <= 8)
+			cuda::SDDMMDotCsr<IdType, DType, 4>(csr, ufeat, vfeat, out);
+		else if (reduce_dim <= 16)
+			cuda::SDDMMDotCsr<IdType, DType, 8>(csr, ufeat, vfeat, out);
+		else if (reduce_dim <= 32)
+			cuda::SDDMMDotCsr<IdType, DType, 16>(csr, ufeat, vfeat, out);
+		else if (reduce_dim <= 64)
+			cuda::SDDMMDotCsr<IdType, DType, 32>(csr, ufeat, vfeat, out);
+		else if (reduce_dim <= 128)
+			cuda::SDDMMDotCsr<IdType, DType, 64>(csr, ufeat, vfeat, out);
+		else if (reduce_dim <= 256)
+			cuda::SDDMMDotCsr<IdType, DType, 128>(csr, ufeat, vfeat, out);
+		else if (reduce_dim <= 512)
+			cuda::SDDMMDotCsr<IdType, DType, 256>(csr, ufeat, vfeat, out);
+		else
+			cuda::SDDMMDotCsr(IdType, DType, 512>(csr, ufeat, vfeat, out);	
+	} else {
+		SWITCH_OP(op, Op, {
+			cuda::SDDMMCsr<IdType, DType, Op>(csr, ufeat, vfeat, out);
+		});
+	}
 }
 
 template <int XPU, typename IdType, typename DType>
@@ -49,9 +70,33 @@ void SDDMMCoo(const std::string& op,
               NDArray vfeat,
               NDArray out,
               std::vector<NDArray> out_aux) {
-  SWITCH_OP(op, Op, {
-    cuda::SDDMMCoo<IdType, DType, Op>(coo, ufeat, vfeat, out);
-  });
+	if (op == "dot") {
+		int64_t reduce_dim = ufeat->shape[ufeat->ndim - 1];
+		if (reduce_dim <= 2)
+			cuda::SDDMMDotCoo<IdType, DType, 1>(coo, ufeat, vfeat, out);
+		else if (reduce_dim <= 4)
+			cuda::SDDMMDotCoo<IdType, DType, 2>(coo, ufeat, vfeat, out);
+		else if (reduce_dim <= 8)
+			cuda::SDDMMDotCoo<IdType, DType, 4>(coo, ufeat, vfeat, out);
+		else if (reduce_dim <= 16)
+			cuda::SDDMMDotCoo<IdType, DType, 8>(coo, ufeat, vfeat, out);
+		else if (reduce_dim <= 32)
+			cuda::SDDMMDotCoo<IdType, DType, 16>(coo, ufeat, vfeat, out);
+		else if (reduce_dim <= 64)
+			cuda::SDDMMDotCoo<IdType, DType, 32>(coo, ufeat, vfeat, out);
+		else if (reduce_dim <= 128)
+			cuda::SDDMMDotCoo<IdType, DType, 64>(coo, ufeat, vfeat, out);
+		else if (reduce_dim <= 256)
+			cuda::SDDMMDotCoo<IdType, DType, 128>(coo, ufeat, vfeat, out);
+		else if (reduce_dim <= 512)
+			cuda::SDDMMDotCoo<IdType, DType, 256>(coo, ufeat, vfeat, out);
+		else
+			cuda::SDDMMDotCoo(IdType, DType, 512>(coo, ufeat, vfeat, out);	
+	} else {
+		SWITCH_OP(op, Op, {
+			cuda::SDDMMCoo<IdType, DType, Op>(coo, ufeat, vfeat, out);
+		});
+	}
 }
 
 template <int XPU, typename IdType, typename DType>
