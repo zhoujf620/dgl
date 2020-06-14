53d15df67c0dd8ef53522b0bebe06b262257968e
yzh119 <expye@outlook.com>
2020-06-15 02:45:19 +0800

    upd

--- tests/compute/test_to_device.py
+++ tests/compute/test_to_device.py
@@ -12,6 +12,17 @@ def test_to_device():
         g = g.to(F.cuda())
         assert g is not None
 
+@unittest.skipIf(F._default_context_str == 'cpu', reason="Need gpu for this test")
+def test_to_device_non_blocking():
+    # to test if DGLGraph.to is compatible with PyTorch Lighting
+    # related issue: https://github.com/dmlc/dgl/issues/1547
+    g = dgl.DGLGraph()
+    g.add_nodes(5, {'h' : F.ones((5, 2))})
+    g.add_edges([0, 1], [1, 2], {'m' : F.ones((2, 2))})
+    if F.is_cuda_available():
+        g = g.to(F.cuda(), non_blocking=True)
+        assert g is not None
 
 if __name__ == '__main__':
     test_to_device()
+    test_to_device_non_blocking()
